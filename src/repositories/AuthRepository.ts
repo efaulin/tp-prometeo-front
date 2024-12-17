751cae935018fc2ccdc4c08765b47c65a00c7458
Eugenio Pandu Faulin <efaulin@frro.utn.edu.ar>
2024-12-17 11:23:22 -0300

    WIP Clase para el manejo del JWT

---
+++ src/repositories/AuthRepository.ts
@@ -0,0 +1,5 @@
+export class AuthRepository {
+    static async Login(username:string, password:string) {
+        //TODO Usar esta funcion para la peticion a "api/auth/login", y preferentemente hacer el proceso de almacenar el token aca tambien.
+    }
+}
